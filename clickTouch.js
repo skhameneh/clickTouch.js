9c9a72eebb505f955e45c70d4a95def69c4cb595
Shawn Khameneh <skhameneh@gmail.com>
2012-02-13 11:56:32 -0600

    First commit of clickTouch.js, v0.1 stable.

---
+++ clickTouch.js
@@ -0,0 +1,42 @@
+var clickTouch = function(){
+	var isDown = false;
+	
+	function createEvent(mouseEvent, name) {
+		var myEvent = document.createEvent('CustomEvent');
+		myEvent.initEvent(name, true, true);
+		myEvent.changedTouches = [mouseEvent];
+		myEvent.identifier = 'mouse';
+
+		(mouseEvent.target?mouseEvent.target:document).dispatchEvent(myEvent);
+	}
+	
+	return {
+		init: function(preventDefault) {
+			function mousedown(event){
+				if(preventDefault)
+					event.preventDefault();
+				isDown = true;
+				clickTouch.createEvent(event, 'touchstart');
+			}
+			document.body.addEventListener('mousedown', mousedown);
+			
+			function mouseup(event){
+				if(preventDefault)
+					event.preventDefault();
+				isDown = false;
+				clickTouch.createEvent(event, 'touchend');
+			}
+			document.body.addEventListener('mouseup', mouseup);
+			
+			function mousemove(event){
+				if(preventDefault)
+					event.preventDefault();
+				if(isDown)
+					clickTouch.createEvent(event, 'touchmove');
+			}
+			document.body.addEventListener('mousemove', mousemove);
+		},
+		createEvent: createEvent
+	}
+
+}();
